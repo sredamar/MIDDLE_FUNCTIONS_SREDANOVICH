3c0b96ba9d939665bf90a14aa85eaaf5bf71ae7a
sredamar <92477629+sredamar@users.noreply.github.com>
2021-10-15 10:37:02 +0300

    Update operations_num_1.cpp

--- operations_num_1.cpp
+++ operations_num_1.cpp
@@ -62,8 +62,13 @@ if (number > 0) {
 int itc_max_num(long long number) {
     int maximum;
     maximum = 0;
-    while (number > 0) {
+if (number < 0) {
+    number = number * (-1);
+while (number > 0) {
         if ((number % 10) > maximum)
             maximum = number % 10;
             number = number / 10;}
-    return maximum;}
+    return maximum;
+} else if ((number < 10) && (number >= 0)) {
+    return number;}
+}
