e20e739f206f67ff39d4e9bce734413e802f81e6
sredamar <92477629+sredamar@users.noreply.github.com>
2021-10-17 12:16:35 +0300

    Update operations_num_2.cpp

--- operations_num_2.cpp
+++ operations_num_2.cpp
@@ -2,8 +2,14 @@
 
 int itc_min_num(long long number) {
     int minimum = 9;
-if (number < 0) {
-    number = number * (-1);
+if (number > 0) {
+        while (number > 0) {
+        if ((number % 10) < minimum)
+            minimum = number % 10;
+    number = number / 10;}
+    return minimum;}
+else if (number < 0) {
+        number = number * (-1);
 while (number > 0) {
         if ((number % 10) < minimum)
             minimum = number % 10;
@@ -17,12 +23,18 @@ int itc_rev_num(long long number) {
   int amount, length;
   amount = 0;
   length = 0;
-if (number < 0) {
-        number = number * (-1);
-  while (number>0) {
-    amount=amount*10+(number%10);
-    length=length+1;
-    number=number/10;}
+if (number > 0) {
+    while (number > 0) {
+    amount = amount * 10 + (number % 10);
+    length = length + 1;
+    number = number / 10;}
+  return itc_len_num(amount);}
+        else if (number < 0) {
+                number = number * (-1);
+  while (number > 0) {
+    amount = amount * 10 + (number % 10);
+    length = length + 1;
+    number = number / 10;}
   return itc_len_num(amount);
 } else if ((number < 10) && (number >= 0)) {
     return 1;}
@@ -31,7 +43,14 @@ if (number < 0) {
 int itc_null_count(long long number) {
     int zero, i;
     zero = 0;
-    if (number < 0) {
+    if (number > 0) {
+            while (number > 0) {
+        i = number % 10;
+        if (i == 0)
+            zero++;
+        number = number / 10;}
+    return zero;}
+    else if (number < 0) {
         number = number * (-1);
     while (number > 0) {
         i = number % 10;
@@ -47,9 +66,9 @@ bool itc_mirror_num(long long number) {
     a = mypow(10, itc_len_num(number) / 2 + 1) + number / mypow(10, itc_len_num(number) / 2 + itc_len_num(number) % 2) * 10 + 1;
     b = mypow(10, itc_len_num(number) / 2 + 1)+ (number % mypow(10, itc_len_num(number) / 2)) * 10 + 1;
     if (a == itc_rev_num_support(b))
-        return "TRUE";
+        return true;
     else
-        return "FALSE";}
+        return false;}
 
 int itc_mirror_count(long long number) {
   int r = 0;
