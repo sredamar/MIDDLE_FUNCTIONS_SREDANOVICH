5282cc7ad1688ea1807f548269fe9508b9052128
sredamar <92477629+sredamar@users.noreply.github.com>
2021-10-17 14:06:20 +0300

    Update operations_num_3.cpp

--- operations_num_3.cpp
+++ operations_num_3.cpp
@@ -1,39 +1,47 @@
 #include "middle.h"
 
 int itc_second_max_num(long long number) {
+    if (number < 0) {
+        number = number * (-1);}
     if (number > 9) {
-    int a = - 1;
-    int b = - 1;
+    int first, second;
+    first = -1;
+    second = -1;
     while (number > 0) {
-        if ((number % 10) > a) {
-            b = a;
-            a = number % 10; }
+        if ((number % 10) > first) {
+            second = first;
+            first = number % 10; }
         else {
-            if ((number % 10) > b) {
-                b = number % 10; } }
+            if ((number % 10) > second) {
+                second = number % 10; } }
         number = number / 10;}
-    return b;}
-    else {
-        return -1;}}
+    return second;}
+    else
+        return -1;}
 
 int itc_second_simple_max_num(long long number) {
+    if (number < 10)
+        return -1;
     if (number > 9) {
-    int a = -1;
-    int b = -1;
+    int first, second;
+    first = -1;
+    second = -1;
     while (number > 0){
-        if ((number % 10) > a){
-            b = a;
-            a = number % 10;}
+        if ((number % 10) >  first){
+             second =  first;
+             first = number % 10;}
         else {
-            if ((number % 10) > b) {
-                b = number % 10; }}
+            if ((number % 10) > second)
+                second = number % 10; }
         number = number / 10; }
-    if (a==b) {
-        return -1;}
-    else {
-        return b;}}}
+    if (first == second)
+        return -1;
+    else
+        return second;}}
 
 long long itc_bin_num(long long number) {
+    if (number < 0){
+        return (itc_bin_num(number * -1))* -1;}
     long long r = 1;
     while (number > 0) {
         r = r * 10 + (number % 2);
@@ -41,17 +49,20 @@ long long itc_bin_num(long long number) {
     return itc_rev_num_support(r) / 10;}
 
 long long itc_oct_num(long long number){
+    if (number < 0){
+        return (itc_oct_num(number * -1))* -1;}
     long long r = 1;
     while (number > 0) {
         r = r * 10 + (number % 8);
         number = number / 8;}
     return itc_rev_num_support(r) / 10;}
 
-int itc_rev_bin_num(long long number) {
-    int r = 0;
-    int k = 0;
-    while (number > 0) {
-        r = r + (number % 10) * mypow(2, k);
-        number = number / 10;
-        k += 1;}
-    return r;}
+int itc_rev_bin_num(long long number){
+    long long bin;
+    bin = 0;
+    int digits;
+    digits = itc_len_num(number);
+    for (int i = 0; i < digits; i++){
+        bin = bin + (number % 10)* mypow(2, i);
+        number = number / 10;}
+    return bin;}
